b51463a06d077b10377c90ab51fc1f549e5b0ef1
srozovolimp-code <srozov.olimp@gmail.com>
2026-05-12 10:31:08 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import gspread
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from gspread import service_account
@@ -26,7 +27,7 @@ if __name__ == "__main__":
         "api_field_list": visit_field_list,
         "google_sheet_url": os.getenv("VISIT_SHEET_URL")
     }]
-    gc = gspread.service_account(filename='metrics-to-sheets-495710-de63a2d35399.json')
+    gc = gspread.service_account()
     for data_elem in data_list:
         data = get_log_data(api_host_url,
                             counter_id,
